1b73a31b75893eba938b093691aca5129f0b4be1
aikpookky <Jeraroj@hotmail.com>
2018-08-28 13:02:41 +0700

    d

---
+++ resources/views/quiz3.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0021)http://www.vru.ac.th/ -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+<title>::: VRU :::</title>
+
+<style type="text/css">
+body {
+	margin-top: 80px;
+	background-color: #6CCDDB;
+}
+.style1 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+}
+a:link {
+	text-decoration: none;
+	color: #000;
+}
+a:visited {
+	text-decoration: none;
+	color: #000;
+}
+a:hover {
+	text-decoration: underline;
+	color: #03F;
+}
+a:active {
+	text-decoration: none;
+	color: #000;
+}
+a {
+	font-size: 16px;
+}
+</style>
+</head>
+
+<body>
+
+<div align="center"><img src="{{url('/')}}/img/head-homepage.png" usemap="#Map" border="0">
+  <map name="Map" id="Map">
+    <area shape="rect" coords="573,28,687,88" href="xxx" target="_blank">
+    <area shape="rect" coords="698,29,832,87" href="xxx" target="_blank">
+    <area shape="rect" coords="851,27,1003,85" href="xxx" target="_blank">
+    <area shape="rect" coords="10,-1,287,111" href="xxx">
+  </map>
+</div>
+<table width="983" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tbody><tr>
+    <td width="348" align="center" scope="col"></td>
+    <td width="635" align="center" scope="col"></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center" scope="col"><a href="http://www.vru.ac.th/index1.php"><img src="{{url('/')}}/img/queen9-61-1.jpg" width="1024" height="321"></a></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="right" scope="col"><a href="http://www.vru.ac.th/index1.php"><img src="{{url('/')}}/img/queen9-61-2.jpg" width="1024" height="199"></a></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center" scope="col">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center" scope="col">&nbsp;</td>
+  </tr>
+</tbody></table>
+
+
+</body></html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function() {
+ 	return '<h1>This is home page</h1>' ;
+});
+Route::get('/blog/{page_id}', function($page_id) {
+     return "<h1>This is blog page : $page_id </h1>" ;
+});
+Route::get('/blog/{page_id}/edit', function($page_id) {
+ 	return "<h1>This is blog page : $page_id for edit</h1>" ;
+});
+Route::get('/product/{a}/{b}/{c}', function($a, $b, $c) {
+ 	return "<h1>This is product page </h1><div>$a , $b, $c</div>" ;
+});
+Route::get('/category/{a?}', function($a = "mobile") {
+ 	return "<h1>This is category page : $a </h1>" ;
+});
+Route::get('/hello', function () {	
+return view('hello');
+});
+Route::get('/greeting', function () {
+	$data = [
+'name'      => 'Jiraroj' ,
+'last_name' => 'Suaysin'
+];
+return view('greeting', $data);
+});
+Route::get('/quiz3', function () {	
+return view('quiz3');
+});
